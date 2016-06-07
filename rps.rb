7c6ba58c3603b451201bf887429063238a46af00
StevenGreenup <greenup.steven@gmail.com>
2016-06-07 15:57:33 -0500

    get.chomp on player one

--- rps.rb
+++ rps.rb
@@ -5,7 +5,7 @@
     def play
       puts "Let's play!"
       puts "Choose ROCK, PAPER, or SCISSORS"
-      @player_one_choice = ["ROCK", "PAPER", "SCISSORS"].sample
+      @player_one_choice = gets.chomp.upcase
       @computer = ["ROCK", "PAPER", "SCISSORS"].sample
         print "Player one selected:  "
         puts player_one_choice
@@ -15,7 +15,7 @@
         if player_one_choice == computer
           puts "It's a draw."
         elsif ((player_one_choice == "ROCK" && computer == "SCISSORS")||(player_one_choice == "PAPER" && computer == "ROCK")||(player_one_choice == "SCISSORS" && computer == "PAPER"))
-          puts "Player one wins." 
+          puts "Player one wins."
         else
           puts "Computer wins"
         end
